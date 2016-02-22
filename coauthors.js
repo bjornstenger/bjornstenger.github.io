20b38d84dbf9afeac7e5ef264e8ca2b967c8b24b
Bjorn Stenger <bjorn@cantab.net>
2016-02-22 17:09:14 +0900

    first webpage commit

---
+++ coauthors.js
@@ -0,0 +1,12 @@
+if (Date.parse(document.lastModified)!=0)
+  document.write('<size=3><font face="Arial,Helvetica" color="black">Last modified: '
+                  + document.lastModified
+                  + ' </size></font>');
+
+
+
+
+
+
+
+
