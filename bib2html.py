a231e2e51c7338465d22e6c2420298318ebdbd01
Stenger, Bjorn | Bjorn | RIT <bjorn.stenger@rakuten.com>
2017-04-14 11:35:59 +0900

    updated pubs

--- bib2html.py
+++ bib2html.py
@@ -2,52 +2,60 @@
 
 '''Convert utility from bibtex to html'''
 
-# ------------------------------------------------
-# Make a html publication list from a bibtex file
-# The html file is strict html 4.01
+# --------------------------------------------------------------------------------
+# Copyright (C) 2011 by Bjørn Ådlandsvik and Björn Stenger
 #
-# Publications appear in the same order as in the bibtex file
-# Not full bibtex parser:
-#   Entries must end with "}" in first column of a new line
-#   No blank lines allowed within an entry
-#   Must have space before and after the "=" in the field definitions
+# Permission is hereby granted, free of charge, to any person obtaining a copy
+# of this software and associated documentation files (the "Software"), to deal
+# in the Software without restriction, including without limitation the rights
+# to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+# copies of the Software, and to permit persons to whom the Software is
+# furnished to do so, subject to the following conditions:
 #
-# Arbitrary text (e.g. html code) can be embedded in the bibtex file
-# prefixed with 'bib2html:'. It will appear in the html file.
-# Such text is ignored if the bibtex file is used with LaTeX.
+# The above copyright notice and this permission notice shall be included in
+# all copies or substantial portions of the Software.
+#
+# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+# IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+# FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+# AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+# LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+# OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+# THE SOFTWARE.
+# --------------------------------------------------------------------------------
+# Python script to convert bibtex to html (4.01)
+#
+# Usage:
+# bib2html [bibfile] [htmlfile]
+#
+# Currently not a full bibtex parser.
+# Restrictions:
+#   entries must end with "}" in first column of a new line
+#   no blank lines allowed within an entry
+#   must have space before and after the "=" in the field definitions
+#   limited special symbols (accents etc.)
 #
 # Fields such as author, year, title, ... are marked with <span class=...>
-# in the html file. Appearance can be controlled with CSS style sheet.
-#
+# in the html file. Appearance can be controlled with a CSS style sheet.
 # Selected authors (for instance yourself, your research group)
 # can be set in the selected_author list (surname only).
 # They will be marked by <span class="selected>.
 #
-# An url field is transferred as [ link ] to the html file,
+# A url field is transferred as [ link ] to the html file,
 # if url is missing and doi is presented a url is created,
 # A pdf field can point to a local pdf file
-# A qdf field is a pdf field that has been turned off to not 
-# provoke the journal owners.
 #
 # Italic text (such as latin species names) may be marked with
 # \emph{ ... } in the bibtex file. This is handled correctly
 # by both bib2html and LaTeX.
 #
-# -----------------------------------------------------
-# 2008-04-28
+# --------------------------------------------------------------------------------
 # Bjørn Ådlandsvik <bjorn@imr.no>
 # Institute of Marine Research
 #
-# 2011-02-24
-# modified by Bjorn Stenger [bjorn@cantab.net]
-# main modifications:
-# - filenames as command line argument
-# - output according to publication type
-# - text formatting changed
-# - removed option to include html code within bibtex file using 'bib2html' prefix
-# 
-# -----------------------------------------------------
-
+# Björn Stenger [bjorn@cantab.net]
+# Rakuten Institute of Technology
+# --------------------------------------------------------------------------------
 
 import os
 import sys
@@ -55,117 +63,87 @@ import re
 import datetime
 import codecs
 
-# ----------------------
-# User settings
-# ----------------------
+# --------------------------------------------------------------------------------
+# user settings
+# --------------------------------------------------------------------------------
 
-# Input bibtex file
-#bibfile   = 'bjornstenger.bib'
-
-# Output html file
-#htmlfile  = 'publications.html'
-
-# Output html encoding
+# output html encoding
 #encoding = 'UTF-8'
 encoding = 'ISO-8859-1'
 
-# Title element in htmlfile
-title = u'Bjorn Stenger - Publications'
+# html page title
+title = u'Publication List'
 
-# List of authors to emphasis
-selected_authors = [u'Stenger,']
+# list of authors for which the font can be changed
+selected_authors = [u'Yournamehere,']
 
-# Where to look for pdf-files
+# location of pdf-files
 #pdfpath = './pdf'
-pdfpath = 'http://mi.eng.cam.ac.uk/~bdrs2/papers/'
+pdfpath = 'http://bjornstenger.github.io/papers/'
 
-# CSS style file to use
+# css style file to use
 css_file = 'style.css'
 
-# -----------------------
-# HTML prolog and epilog
-# -----------------------
-
+# html prolog
+# modify according to your needs
 prolog = """<!DOCTYPE HTML
     PUBLIC "-//W3C//DTD HTML 4.01//EN"
     "http://www.w3.org/TR/html4/strict.dtd">
-    
 <head>
   <meta http-equiv=Content-Type content="text/html; charset=%s">
   <title>%s</title>
-
   <link rel="stylesheet" type="text/css" href="%s">
-
-
-
-  <style type="text/css">     
+  <style type="text/css">
      span.selected {color: #000000}
      span.author {color: #000000}
      span.title {font-weight: bold;}
      li {margin-top: 0px; margin-bottom: 16px}
   </style>
-
-
-<!--
-
-     li {margin-top: 0px; margin-bottom: 16px}
-
-     body{
-       width:700px;
-       margin:0px auto;
-       text-align:justify;
-       padding:25px;
-       font-family: Verdana, Arial, Helvetica, sans-serif;
-	font-family: Trebuchet MS,Verdana, Arial, Helvetica, sans-serif;
-
-	font-size:10px;
-	font: 0.71em Verdana;
-	line-height: 1.7;
-        color: #404040;
-      }
-   </style>
--->
-
-
 </head>
-
 <body>
 
 <div class="header">
-<strong>&nbsp;&nbsp;&nbsp;Bj&ouml;rn Stenger</strong><br> 
-&nbsp;&nbsp;&nbsp;Computer Vision Group, Toshiba Research Europe
+<strong>&nbsp;&nbsp;&nbsp;Bj&ouml;rn Stenger</strong><br>
 </div>
 
 <div class="container">
 <ul id="miniflex">
 	<li><a href="index.html">home</a></li>
+	<li><a href="research.html">research</a></li>
 	<li><a href="pubs.html" class="active">publications</a></li>
 </ul>
 </div>
 
-
 <div id="content">
+<h2>
+[<a href="bjornstenger.bib"  target="_top">BibTeX file</a>]&nbsp;
+</h2>
+<br>
 """ % (encoding, title, css_file)
 
 # now = yyyy-mm-dd
 now = str(datetime.date.today())
 
+# html epilog
 epilog = """
 <br>
 <hr>
 Created %s with <a href="bib2html.py">bib2html.py</a>
 </p>
 </div>
-
 </body>
 </html>
 """ % (now)
 
 
-# -------------------------------
-# Class and function definitions
-# -------------------------------
 
+
+
+
+
+# --------------------------------------------------------------------------------
+# class and function definitions
+# --------------------------------------------------------------------------------
 
 # regular expression for \emph{...{...}*...}
 emph = re.compile(u'''
@@ -175,10 +153,11 @@ emph = re.compile(u'''
 
 
 
+# --------------------------------------------------------------------------------
+# class: bibtex entry
+# --------------------------------------------------------------------------------
 class Entry(object):
     """Class for bibtex entry"""
-
-    # -------------------
 
     def clean(self):
         '''Clean up an entry'''
@@ -187,69 +166,79 @@ class Entry(object):
             # remove leading and trailing whitespace
             v = v.strip()
 
-            # Fix scandinavian characters
+            # replace special characters - add more if necessary
             v = v.replace('\\AE', u'Æ')
             v = v.replace('\\O',  u'Ø')
             v = v.replace('\\AA', u'Å')
             v = v.replace('\\ae', u'æ')
             v = v.replace('\\o',  u'ø')
             v = v.replace('\\aa', u'å')
-
-            # more special symbols
-            #v = v.replace('&', u'&amp;')
             v = v.replace('\\\'a', '&aacute;')
+            v = v.replace('\\\'e', '&eacute;')
             v = v.replace('\\c{c}' , '&ccedil;')
 
-            # Fix \emph in title
+            # fix \emph in title
             if k == 'title':
                 v = re.sub(emph, '<I>\g<emph_text></I>', v)
 
-            # Remove "{" and "}"
+            # remove "{" and "}"
             v = v.replace('{', '')
             v = v.replace('}', '')
             v = v.replace('"', '')
-        
-            # remove trailing comma and dot
-            if v[-1] == ',': v = v[:-1]
-            #if v[-1] == '.': v = v[:-1]
 
+            # remove trailing comma and dot
+            if len(str(v))>0:
+              if v[-1] == ',': v = v[:-1]
 
             # fix author
             if k == 'author':
 
-                # Split into list of authors
+                # split into list of authors
                 authors = v.split(' and ')
 
-                # Strip each author
+                # strip each author ;)
                 authors = [a.strip() for a in authors]
 
-                # Make blanks non-breakable
+                # make blanks non-breakable
                 authors = [a.replace(' ', '&nbsp;') for a in authors]
 
-                # Mark selected authors
+                # reverse first and surname
                 for i, a in enumerate(authors):
-                    surname = a.split('&nbsp;')[0]
-                    #print surname + '\n'
-                    if surname in selected_authors:
-                        a = ''.join(['<span class="selected">', a, '</span>'])
-                        authors[i] = a
+                    #print a + "\n"
+                    #surname =
+                    namearray = a.split('&nbsp;')
+                    surname = namearray[0]
+                    surname = surname.replace(',', '')
+                    firstname = ' '.join(namearray[1:])
+                    authors[i] = firstname + " " + surname
 
-                # Concatenate the authors again
-                if len(authors) == 1:
-                    v = authors[0]
-                elif len(authors) == 2:
-                    v = authors[0] + " and " + authors[1]
-                else:  # len(authors) > 2:
-                    v =  ", ".join(authors[:-1]) + " and " + authors[-1]
+
+                # mark selected authors - if there are any
+                #for i, a in enumerate(authors):
+                #    surname = a.split('&nbsp;')[0]
+                #    if surname in selected_authors:
+                #        a = ''.join(['<span class="selected">', a, '</span>'])
+                #        authors[i] = a
+
+
+
+                # concatenate the authors again
+                #if len(authors) == 1:
+                #    v = authors[0]
+                #elif len(authors) == 2:
+                #    v = authors[0] + " and " + authors[1]
+                #else:  # len(authors) > 2:
+                #    v =  ", ".join(authors[:-1]) + " and " + authors[-1]
+                v = ", ".join(authors[:])
 
             # fix pages
             if k == 'pages':
                 v = v.replace('--', '&ndash;')
                 v = v.replace('-',  '&ndash;')
-        
+
             setattr(self, k, v)
-        
-    # ------------------ 
+
+    # ------------------
 
     def write(self, fid):
         """Write entry to html file"""
@@ -260,34 +249,40 @@ class Entry(object):
         fid.write('\n')
         fid.write('<li>\n')
 
+        # --- chapter ---
+        chapter = False
+        if edict.has_key('chapter'):
+          chapter = True
+          fid.write('<span class="title">')
+          fid.write(self.chapter)
+          fid.write('</span>')
+          fid.write(',<br>')
+
+
+
+        # --- title ---
+        if not(chapter):
+            fid.write('<span class="title">')
+            fid.write(self.title)
+            fid.write('</span>')
+            fid.write(',<br>')
+
+
         # --- author ---
         fid.write('<span class="author">')
         fid.write(self.author)
         fid.write('</span>')
         fid.write(',<br>')
 
-        # --- title ---
-        #fid.write('<span class="title">')
-        #fid.write(self.title)
-        #fid.write('</span>')
-        #fid.write(',<br>')
-
-        # --- title with link ---
-        fid.write('<span class="title">')
-        fid.write(self.title)
-        fid.write('</span>')
-        fid.write(',<br>')
-
-
-        # --- chapter ---
-        if edict.has_key('chapter'):
-          fid.write(self.chapter)
-          fid.write(', in: ')
+        # -- if book chapter --
+        if chapter:
+          fid.write('in: ')
           fid.write('<i>')
           fid.write(self.title)
           fid.write('</i>')
           fid.write(', ')
           fid.write(self.publisher)
+
 
         # --- journal or similar ---
         journal = False
@@ -307,8 +302,8 @@ class Entry(object):
             journal = True
             fid.write('Tech. Report, ')
             fid.write(edict['number'])
-    
-        # --- Volume, pages, notes or similar ---
+
+        # --- volume, pages, notes etc ---
         if edict.has_key('volume'):
             fid.write(', Vol. ')
             fid.write(edict['volume'])
@@ -319,7 +314,7 @@ class Entry(object):
                 fid.write(', p.')
                 fid.write(edict['pages'])
         elif edict.has_key('note'):
-            if journal: fid.write(', ')
+            if journal or chapter: fid.write(', ')
             fid.write(edict['note'])
         if edict.has_key('month'):
             fid.write(', ')
@@ -327,12 +322,13 @@ class Entry(object):
 
         # --- year ---
         fid.write('<span class="year">')
-        fid.write(', ');
+        #fid.write(', ');
+        fid.write(' ');
         fid.write(self.year)
         fid.write('</span>')
         #fid.write(',\n')
 
-        # Final period
+        # final period
         fid.write('.')
 
         # --- Links ---
@@ -363,8 +359,12 @@ class Entry(object):
         # Terminate the list entry
         fid.write('</li>\n')
 
-# ---------------------------------------
 
+
+
+# --------------------------------------------------------------------------------
+# generator: bibtex reader
+# --------------------------------------------------------------------------------
 def bib_reader(filename):
     '''Generator for iteration over entries in a bibtex file'''
 
@@ -389,11 +389,11 @@ def bib_reader(filename):
 
             # Iterate through the fields of the entry
             first_field = True
-            while True: 
+            while True:
                 line = fid.next()
                 #print line + '\n'
                 words = line.split()
-                
+
                 if words[0] == "}": # end of entry
                     # store last field
                     setattr(e, fieldname, fieldtext)
@@ -412,52 +412,52 @@ def bib_reader(filename):
                 else:  # continued line
                     fieldtext = " ".join([fieldtext] + words)
 
-                
         yield e
-        
 
-# --------------------------------------        
-# Main action
-# --------------------------------------        
 
+
+
+# --------------------------------------------------------------------------------
+# main
+# --------------------------------------------------------------------------------
 def main():
 
   args = sys.argv[1:]
 
   if not args:
-    print 'usage: bib2html [bibfile] [htmlfile]'
-    sys.exit(1)
-
-  bibfile=args[0]
-  
-  del args[0:1]
+      bibfile="bjornstenger.bib"
+    #print 'usage: bib2html [bibfile] [htmlfile]'
+    #sys.exit(1)
+  else:
+      bibfile=args[0]
+      del args[0:1]
 
   if not args:
-    htmlfile="publications.html" #default
+    htmlfile="publist.html" #default
   else:
     htmlfile=args[0]
 
-  # Create the html file with opted encoding 
+
+  # create the html file with opted encoding
   f1 = codecs.open(htmlfile, 'w', encoding=encoding)
 
-  # Write the initial part of the file
+  # write the initial part of the file
   f1.write(prolog)
 
-  # Open the bibtex file
+  # lists according to publication type
   bookchapterlist =[]
   journallist =[]
   conflist =[]
   techreportlist =[]
   thesislist =[]
 
-  #f0 = bib_reader(bibfile, elist)
+
+  # read bibtex file
   f0 = bib_reader(bibfile)
 
    # Iterate over the entries and bib2html directives
-  in_list = False       # in/out of unordered list
-  for e in f0:  
+  for e in f0:
     e.clean()
-    #print "type:" + e.type +"\n"
     if (e.type=="inbook"):
       bookchapterlist.append(e)
     if (e.type=="article"):
@@ -469,45 +469,43 @@ def main():
     if (e.type=="phdthesis"):
       thesislist.append(e)
 
+  # write list according to publication type
 
-  f1.write('<h2>Book Chapters</h2>');
-  f1.write('\n<ol>\n\n')
-  for e in bookchapterlist:
-    e.write(f1)
-  f1.write('\n</ol>\n\n')
-
-  f1.write('<h2>Journal Publications</h2>');
-  f1.write('\n<ol>\n\n')
+  f1.write('<h2>Journals</h2>');
+  f1.write('\n<ol reversed>\n\n')
   for e in journallist:
     e.write(f1)
   f1.write('\n</ol>\n\n')
 
-  f1.write('<h2>Conference Publications</h2>');
-  f1.write('\n<ol>\n\n')
+  f1.write('<h2>Conferences and Workshops</h2>');
+  f1.write('\n<ol reversed>\n\n')
   for e in conflist:
     e.write(f1)
   f1.write('\n</ol>\n\n')
 
+  f1.write('<h2>Book Chapters</h2>');
+  f1.write('\n<ol reversed>\n\n')
+  for e in bookchapterlist:
+    e.write(f1)
+  f1.write('\n</ol>\n\n')
+
   f1.write('<h2>Technical Reports</h2>');
-  f1.write('\n<ol>\n\n')
+  f1.write('\n<ol reversed>\n\n')
   for e in techreportlist:
     e.write(f1)
   f1.write('\n</ol>\n\n')
 
-  f1.write('<h2>Theses</h2>');
-  f1.write('\n<ol>\n\n')
+  f1.write('<h2>Thesis</h2>');
+  f1.write('\n<ol reversed>\n\n')
   for e in thesislist:
     e.write(f1)
   f1.write('\n</ol>\n\n')
-    
 
-  # Finish the html file
+  # write epilog
   f1.write(epilog)
   f1.close()
-  
-  print 'Written: '+htmlfile+'\n'
 
-
+  print 'written: '+htmlfile+'\n'
 
 if __name__ == '__main__':
   main()
